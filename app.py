86f7f6e32de1ab862d36ce0e9ba6fc23a90d42c8
gracy452 <gracysoni48@gmail.com>
2026-01-31 16:42:24 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import pickle
 import numpy as np
+import os
 
-model = pickle.load(open("sleep_model.pkl", "rb"))
-scaler = pickle.load(open("scaler.pkl", "rb"))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model_path = os.path.join(BASE_DIR, "sleep_model.pkl")
+scaler_path = os.path.join(BASE_DIR, "scaler.pkl")
+
+model = pickle.load(open(model_path, "rb"))
+scaler = pickle.load(open(scaler_path, "rb"))
 
 st.title("🛌 Sleep Disorder Prediction System")
 
